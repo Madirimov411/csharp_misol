90c676f1a607a332c4038f8eda185bf4afe6afee
Lenovo <Lenovo@DESKTOP-CNO9ICK>
2023-03-18 13:35:43 +0500

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ConsoleApp18
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Person> person = new List<Person>()
+            {
+                new Person("Farrux","Shonazarov","Muzaffar o'g'li",20),
+                new Person("Zaynabbegim","Ollaberganova","Dilmurod qizi",19)
+            };
+            var name = person.Select(p => new
+            {
+                fullName = $"{p.lName} {p.fName} {p.mName}",
+                year = DateTime.Now.Year - p.age
+            });
+            foreach (var i in name)
+            {
+                Console.WriteLine($"{i.fullName} - {i.year}-yil ");
+            }
+
+            Console.ReadKey();
+        }
+
+
+        class Person
+        {
+            public string fName { get; set; }
+            public string lName { get; set; }
+            public string mName { get; set; }
+            public int age { get; set; }
+
+            public Person(string fName, string lName, string mName, int age)
+            {
+                this.fName = fName;
+                this.lName = lName;
+                this.mName = mName;
+                this.age = age;
+            }
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("ConsoleApp18")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ConsoleApp18")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("6ae6a864-60e0-469f-add9-39e2c562559c")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
